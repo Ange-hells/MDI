2e16387200cf5d2b2cd09c35094621ec7d1fb195
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-03-14 19:53:31 +0100

    Add Primary 007 game in MDI

---
+++ Application MDI/007.vb
@@ -0,0 +1,19 @@
+﻿Public Class _007
+
+    Dim O_vie As Byte = 3
+    Dim J_vie As Byte = 3
+    Dim O_coup As Byte
+    Dim J_coup As Byte
+    Dim O_charge As Byte = 0
+    Dim J_charge As Byte = 0
+    'Dim Coup As Array
+
+    Public Sub Play007()
+        While O_vie <> 0 And J_vie <> 0
+
+
+        End While
+
+    End Sub
+
+End Class
