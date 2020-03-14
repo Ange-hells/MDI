a6b1434d11b94eed7beb7d66bada275a860bc9df
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-03-14 19:53:58 +0100

    Add primary Chifumi game in MDI

---
+++ Application MDI/Chifumi.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Chifumi
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BT_Pierre = New System.Windows.Forms.Button()
+        Me.BT_Papier = New System.Windows.Forms.Button()
+        Me.BT_Ciseau = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BT_Pierre
+        '
+        Me.BT_Pierre.Location = New System.Drawing.Point(283, 222)
+        Me.BT_Pierre.Name = "BT_Pierre"
+        Me.BT_Pierre.Size = New System.Drawing.Size(75, 23)
+        Me.BT_Pierre.TabIndex = 0
+        Me.BT_Pierre.Text = "Pierre"
+        Me.BT_Pierre.UseVisualStyleBackColor = True
+        '
+        'BT_Papier
+        '
+        Me.BT_Papier.Location = New System.Drawing.Point(283, 269)
+        Me.BT_Papier.Name = "BT_Papier"
+        Me.BT_Papier.Size = New System.Drawing.Size(75, 23)
+        Me.BT_Papier.TabIndex = 1
+        Me.BT_Papier.Text = "Papier"
+        Me.BT_Papier.UseVisualStyleBackColor = True
+        '
+        'BT_Ciseau
+        '
+        Me.BT_Ciseau.Location = New System.Drawing.Point(283, 314)
+        Me.BT_Ciseau.Name = "BT_Ciseau"
+        Me.BT_Ciseau.Size = New System.Drawing.Size(75, 23)
+        Me.BT_Ciseau.TabIndex = 2
+        Me.BT_Ciseau.Text = "Ciseaux"
+        Me.BT_Ciseau.UseVisualStyleBackColor = True
+        '
+        'Chifumi
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(509, 486)
+        Me.Controls.Add(Me.BT_Ciseau)
+        Me.Controls.Add(Me.BT_Papier)
+        Me.Controls.Add(Me.BT_Pierre)
+        Me.Name = "Chifumi"
+        Me.Text = "Chifumi"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BT_Pierre As Button
+    Friend WithEvents BT_Papier As Button
+    Friend WithEvents BT_Ciseau As Button
+End Class
---
+++ Application MDI/Chifumi.vb
@@ -0,0 +1,42 @@
+﻿Public Class Chifumi
+
+    Dim O_scor As Byte = 3
+    Dim J_scor As Byte = 3
+    Dim O_coup As Byte
+    Dim J_coup As Byte
+    'Dim Coup As Array
+
+    Public Sub PlayChifumi(J_coup)
+        While O_scor <> 0 And J_scor <> 0
+            O_coup = O_Alea_Coup()
+            If O_coup <> J_coup Then
+                If (O_coup = 0 And J_coup = 1) Or (O_coup = 1 And J_coup = 2) Or (O_coup = 2 And J_coup = 0) Then
+                    J_scor += 1
+                ElseIf (J_coup = 0 And O_coup = 1) Or (J_coup = 1 And O_coup = 2) Or (J_coup = 2 And O_coup = 0) Then
+                    O_scor += 1
+                End If
+            End If
+
+        End While
+
+    End Sub
+
+    Private Function O_Alea_Coup()
+        Dim numO_coup As Byte
+        Randomize()
+
+        Return numO_coup
+    End Function
+
+    Private Sub BT_Pierre_Click(sender As Object, e As EventArgs) Handles BT_Pierre.Click
+        J_coup = 0
+    End Sub
+
+    Private Sub BT_Papier_Click(sender As Object, e As EventArgs) Handles BT_Papier.Click
+        J_coup = 1
+    End Sub
+
+    Private Sub BT_Ciseau_Click(sender As Object, e As EventArgs) Handles BT_Ciseau.Click
+        J_coup = 2
+    End Sub
+End Class
