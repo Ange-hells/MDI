52c81f9718a68a5daa05f62d60d254607ee74f01
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-03-22 18:10:28 +0100

    Update 007.Designer.vb

--- Application MDI/007.Designer.vb
+++ Application MDI/007.Designer.vb
@@ -22,16 +22,301 @@ Partial Class _007
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PBJ_V1 = New System.Windows.Forms.PictureBox()
+        Me.PBJ_V2 = New System.Windows.Forms.PictureBox()
+        Me.PBJ_V3 = New System.Windows.Forms.PictureBox()
+        Me.PBO_V3 = New System.Windows.Forms.PictureBox()
+        Me.PBO_V1 = New System.Windows.Forms.PictureBox()
+        Me.PBO_V2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.BT_Tirer = New System.Windows.Forms.Button()
+        Me.BT_Charger = New System.Windows.Forms.Button()
+        Me.BT_Proteger = New System.Windows.Forms.Button()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PBJ_V1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PBJ_V2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PBJ_V3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PBO_V3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PBO_V1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PBO_V2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Application_MDI.My.Resources.Resources.wanted
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 355)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(193, 280)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = Global.Application_MDI.My.Resources.Resources.wanted
+        Me.PictureBox2.Location = New System.Drawing.Point(211, 355)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(193, 280)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PBJ_V1
+        '
+        Me.PBJ_V1.BackColor = System.Drawing.Color.Transparent
+        Me.PBJ_V1.Image = Global.Application_MDI.My.Resources.Resources.icons8_coeurs_48
+        Me.PBJ_V1.Location = New System.Drawing.Point(248, 600)
+        Me.PBJ_V1.Name = "PBJ_V1"
+        Me.PBJ_V1.Size = New System.Drawing.Size(24, 24)
+        Me.PBJ_V1.TabIndex = 2
+        Me.PBJ_V1.TabStop = False
+        '
+        'PBJ_V2
+        '
+        Me.PBJ_V2.Image = Global.Application_MDI.My.Resources.Resources.icons8_coeurs_48
+        Me.PBJ_V2.Location = New System.Drawing.Point(296, 600)
+        Me.PBJ_V2.Name = "PBJ_V2"
+        Me.PBJ_V2.Size = New System.Drawing.Size(24, 24)
+        Me.PBJ_V2.TabIndex = 3
+        Me.PBJ_V2.TabStop = False
+        '
+        'PBJ_V3
+        '
+        Me.PBJ_V3.Image = Global.Application_MDI.My.Resources.Resources.icons8_coeurs_48
+        Me.PBJ_V3.Location = New System.Drawing.Point(343, 600)
+        Me.PBJ_V3.Name = "PBJ_V3"
+        Me.PBJ_V3.Size = New System.Drawing.Size(24, 24)
+        Me.PBJ_V3.TabIndex = 4
+        Me.PBJ_V3.TabStop = False
+        '
+        'PBO_V3
+        '
+        Me.PBO_V3.Image = Global.Application_MDI.My.Resources.Resources.icons8_coeurs_48
+        Me.PBO_V3.Location = New System.Drawing.Point(142, 600)
+        Me.PBO_V3.Name = "PBO_V3"
+        Me.PBO_V3.Size = New System.Drawing.Size(24, 24)
+        Me.PBO_V3.TabIndex = 5
+        Me.PBO_V3.TabStop = False
+        '
+        'PBO_V1
+        '
+        Me.PBO_V1.BackColor = System.Drawing.Color.Transparent
+        Me.PBO_V1.BackgroundImage = Global.Application_MDI.My.Resources.Resources.icons8_coeurs_48
+        Me.PBO_V1.Location = New System.Drawing.Point(51, 600)
+        Me.PBO_V1.Name = "PBO_V1"
+        Me.PBO_V1.Size = New System.Drawing.Size(24, 24)
+        Me.PBO_V1.TabIndex = 6
+        Me.PBO_V1.TabStop = False
+        '
+        'PBO_V2
+        '
+        Me.PBO_V2.BackColor = System.Drawing.Color.Transparent
+        Me.PBO_V2.Image = Global.Application_MDI.My.Resources.Resources.icons8_coeurs_48
+        Me.PBO_V2.Location = New System.Drawing.Point(96, 600)
+        Me.PBO_V2.Name = "PBO_V2"
+        Me.PBO_V2.Size = New System.Drawing.Size(24, 24)
+        Me.PBO_V2.TabIndex = 7
+        Me.PBO_V2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(250, 581)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 16)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = " Freagrim DASTE "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(48, 581)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 16)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "  Hamward TOCY  "
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.Application_MDI.My.Resources.Resources.DASTE
+        Me.PictureBox9.Location = New System.Drawing.Point(251, 436)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(116, 142)
+        Me.PictureBox9.TabIndex = 10
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Image = Global.Application_MDI.My.Resources.Resources.TOCY
+        Me.PictureBox10.Location = New System.Drawing.Point(51, 436)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(115, 142)
+        Me.PictureBox10.TabIndex = 11
+        Me.PictureBox10.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(152, 563)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(14, 15)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(352, 563)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(14, 15)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "0"
+        '
+        'BT_Tirer
+        '
+        Me.BT_Tirer.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_Tirer.Location = New System.Drawing.Point(481, 442)
+        Me.BT_Tirer.Name = "BT_Tirer"
+        Me.BT_Tirer.Size = New System.Drawing.Size(111, 59)
+        Me.BT_Tirer.TabIndex = 14
+        Me.BT_Tirer.Text = "Tirer"
+        Me.BT_Tirer.UseVisualStyleBackColor = True
+        '
+        'BT_Charger
+        '
+        Me.BT_Charger.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_Charger.Location = New System.Drawing.Point(481, 376)
+        Me.BT_Charger.Name = "BT_Charger"
+        Me.BT_Charger.Size = New System.Drawing.Size(111, 60)
+        Me.BT_Charger.TabIndex = 15
+        Me.BT_Charger.Text = "Charger"
+        Me.BT_Charger.UseVisualStyleBackColor = True
+        '
+        'BT_Proteger
+        '
+        Me.BT_Proteger.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_Proteger.Location = New System.Drawing.Point(481, 507)
+        Me.BT_Proteger.Name = "BT_Proteger"
+        Me.BT_Proteger.Size = New System.Drawing.Size(111, 60)
+        Me.BT_Proteger.TabIndex = 16
+        Me.BT_Proteger.Text = "Proteger"
+        Me.BT_Proteger.UseVisualStyleBackColor = True
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.Image = Global.Application_MDI.My.Resources.Resources.icons8_tirer_48
+        Me.PictureBox11.Location = New System.Drawing.Point(427, 453)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox11.TabIndex = 17
+        Me.PictureBox11.TabStop = False
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.Image = Global.Application_MDI.My.Resources.Resources.icons8_balle_2_48
+        Me.PictureBox12.Location = New System.Drawing.Point(427, 388)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox12.TabIndex = 18
+        Me.PictureBox12.TabStop = False
+        '
+        'PictureBox13
+        '
+        Me.PictureBox13.Image = Global.Application_MDI.My.Resources.Resources.icons8_bouclier_chevalier_48
+        Me.PictureBox13.Location = New System.Drawing.Point(427, 519)
+        Me.PictureBox13.Name = "PictureBox13"
+        Me.PictureBox13.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox13.TabIndex = 19
+        Me.PictureBox13.TabStop = False
         '
         '_007
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(509, 486)
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = Global.Application_MDI.My.Resources.Resources.font_0071
+        Me.ClientSize = New System.Drawing.Size(895, 647)
+        Me.Controls.Add(Me.PictureBox13)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.BT_Proteger)
+        Me.Controls.Add(Me.BT_Charger)
+        Me.Controls.Add(Me.BT_Tirer)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PBJ_V1)
+        Me.Controls.Add(Me.PBJ_V3)
+        Me.Controls.Add(Me.PBJ_V2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PBO_V2)
+        Me.Controls.Add(Me.PBO_V1)
+        Me.Controls.Add(Me.PBO_V3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "_007"
         Me.Text = "_007"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PBJ_V1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PBJ_V2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PBJ_V3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PBO_V3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PBO_V1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PBO_V2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PBJ_V1 As PictureBox
+    Friend WithEvents PBJ_V2 As PictureBox
+    Friend WithEvents PBJ_V3 As PictureBox
+    Friend WithEvents PBO_V3 As PictureBox
+    Friend WithEvents PBO_V1 As PictureBox
+    Friend WithEvents PBO_V2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BT_Tirer As Button
+    Friend WithEvents BT_Charger As Button
+    Friend WithEvents BT_Proteger As Button
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
 End Class
